4c4fa420d453dc78c6e2435e0966ea3d68cee5d2
Mohamed Abdessmad <mabdessm@c2r8p3.42nice.fr>
2024-05-03 12:22:33 +0200

    Final Bonus Commit

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mabdessm <mabdessm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 17:31:43 by mabdessm          #+#    #+#             */
-/*   Updated: 2024/05/01 17:04:05 by mabdessm         ###   ########.fr       */
+/*   Updated: 2024/05/03 11:51:29 by mabdessm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (s1[i] != '\0' && i < n)
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	if (i >= n)
 		return (0);
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
